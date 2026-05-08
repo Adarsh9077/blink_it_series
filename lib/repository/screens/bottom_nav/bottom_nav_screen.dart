8d4ab90f064b782350def18d91fd60018b9e3983
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-05-09 00:29:52 +0530

    add bottom nav bar

--- lib/repository/screens/bottom_nav/bottom_nav_screen.dart
+++ lib/repository/screens/bottom_nav/bottom_nav_screen.dart
@@ -2,6 +2,7 @@ import 'package:blinkit_series/repository/screens/cart/cart_screen.dart';
 import 'package:blinkit_series/repository/screens/category/category_screen.dart';
 import 'package:blinkit_series/repository/screens/home/home_screen.dart';
 import 'package:blinkit_series/repository/screens/print/print_screen.dart';
+import 'package:blinkit_series/repository/widgets/ui_helper.dart';
 import 'package:flutter/material.dart';
 
 class BottomNavScreen extends StatefulWidget {
@@ -19,10 +20,25 @@ class _BottomNavScreenState extends State<BottomNavScreen> {
     CategoryScreen(),
     PrintScreen(),
   ];
+  List<String> items = [
+    "home_icon.png",
+    "shopping_bag_icon.png",
+    "category_icon.png",
+    "printer_icon.png",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(bottomNavigationBar: BottomNavigationBar(items: []));
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          // ...[for item in items{
+          for (int i = 0; i < items.length; i++)
+            BottomNavigationBarItem(icon: UiHelper.customImage(img: items[i])),
+          // }
+          // ]
+        ],
+      ),
+    );
   }
 }
