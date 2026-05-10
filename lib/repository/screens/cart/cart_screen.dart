01bd655edc452310041884db8bb1bac5bde48c7d
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-05-10 23:23:51 +0530

    Working on cart screen

--- lib/repository/screens/cart/cart_screen.dart
+++ lib/repository/screens/cart/cart_screen.dart
@@ -11,16 +11,24 @@ class CartScreen extends StatelessWidget {
         child: Column(
           children: [
             Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
               width: double.infinity,
               height: 100,
               color: Color(0xfff7cb45),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   UiHelper.customText(
                     text: "Blinkit in",
                     color: Colors.black,
                     fontWeight: FontWeight.bold,
                     fontSize: 12,
+                  ),
+                  UiHelper.customText(
+                    text: "16 minutes",
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22,
                   ),
                 ],
               ),
